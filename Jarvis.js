f24e3d5b609fd1e090d5a2afd0c9b7d1c6c6a204
SpeedsterTech <ssspeedstertech@gmail.com>
2026-04-17 15:47:26 -0400

    tried reDirect

--- Jarvis.js
+++ Jarvis.js
@@ -5,12 +5,7 @@ const multer = require('multer');
 const app = express()
 app.use (express.json());
 app.use(express.static('public'));
-<<<<<<< HEAD
-let newGname = "";
-
-=======
 let newGname;
->>>>>>> 12fdd2400bb9ce8b97492053b1cbe7635d5cf6e9
 
 app.post('/api/action', (req,res) => {
   const body = req.body;
@@ -56,11 +51,6 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     //console.log(file)
     x = file.originalname.split(".")
-<<<<<<< HEAD
-    newGname = String(file.fieldname+ "-" +x[0] +"." + x[x.length-1])
-    cb(null,newGname);
-  },
-=======
     newGname = file.fieldname+ "-" +file.originalname +"." + x[x.length-1]
     cb(null,newGname);
     fs.appendFile('games.csv', newGname + "\n", (err) => {
@@ -69,7 +59,6 @@ const storage = multer.diskStorage({
       }
     })
   }
->>>>>>> 12fdd2400bb9ce8b97492053b1cbe7635d5cf6e9
 });
 
 const upload = multer({ storage: storage })
@@ -77,20 +66,12 @@ const upload = multer({ storage: storage })
 app.post('/upload',upload.single("file"), (req,res) => {
   newGname=req.file
   console.log("lets go")
+  res.json("Oh Joy!")
 });
 app.post('/api/game', (req,res) => {
   body = req.body
   console.log(body.name)
   console.log(newGname)
-<<<<<<< HEAD
-  fs.appendFile("game.csv", body.name + ","+ newGname + "\n", (err) => {
-    if(err) {
-      console.log(err);
-    }else{
-      console.log("success");
-    }
-  });
-=======
   fs.appendFile('games.csv',body.name +",", (err) =>{
     if(err){
       console.log(err)
@@ -98,7 +79,9 @@ app.post('/api/game', (req,res) => {
       console.log("Game Logged!")
     }
   })
->>>>>>> 12fdd2400bb9ce8b97492053b1cbe7635d5cf6e9
 });
+app.get('/upload',(req,res) => {
+  res.redirect('public/Index.html')
+})
 app.listen(3000, () => {
 })
