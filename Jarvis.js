77ff99e99b3dcebba8ffe62ef24329121e4260f3
SpeedsterTech <ssspeedstertech@gmail.com>
2026-05-03 16:37:13 -0400

    Page redirects and Game Library Images work

--- Jarvis.js
+++ Jarvis.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const app = express()
 app.use (express.json());
 app.use(express.static('public'));
+app.use(express.static('uploads'));
 let newGname;
 
 app.post('/api/action', (req,res) => {
@@ -66,7 +67,7 @@ const upload = multer({ storage: storage })
 app.post('/upload',upload.single("file"), (req,res) => {
   newGname=req.file
   console.log("lets go")
-  res.json("Oh Joy!")
+  res.redirect('gameLib.html')
 });
 app.post('/api/game', (req,res) => {
   body = req.body
@@ -80,8 +81,5 @@ app.post('/api/game', (req,res) => {
     }
   })
 });
-app.get('/upload',(req,res) => {
-  res.redirect('public/Index.html')
-})
 app.listen(3000, () => {
 })
--- public/game.js
+++ public/game.js
@@ -5,12 +5,16 @@ let gpic = document.getElementById("file")
 submit.addEventListener("click", function newGame(){
    let send = {}
    send.name = gname.value
+    window.location.href = "/gamelib.html";
  const request = new Request("/api/game", {
     method: "POST",
     headers: {'Content-Type' : "application/json"},
     body: JSON.stringify(send)
+   
  })
+  
  const response = fetch(request).then((responce) => responce.json()).then(data => {
     console.log(data);
   })
+
 })
